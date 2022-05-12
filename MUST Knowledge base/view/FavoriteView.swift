d0b616fbabc67469331cb6857ab91be6a3ba0ca8
AymanOmara <75618602+AymanOmara@users.noreply.github.com>
2022-05-12 17:12:23 +0200

    start add when the favorite is empty

--- MUST Knowledge base/view/FavoriteView.swift
+++ MUST Knowledge base/view/FavoriteView.swift
@@ -9,22 +9,33 @@ import SwiftUI
 
 struct FavoriteView: View {
     @StateObject private var viewModel = FavoriteViewModel()
+    @State var isEmpty = false
+    
     var body: some View {
-        List(viewModel.courses,id:\.id) { item in
-            FavoriteCell(viewModel: viewModel, item: item)
-            
-                .navigationTitle("Courses")
-                .navigationBarTitleDisplayMode(.inline)
+        if isEmpty{
+            LottieView(fileName: "empty")
+                .frame(width: 300, height: 300, alignment: .center)
+        }else{
+            List(viewModel.courses,id:\.id) { item in
+                FavoriteCell(viewModel: viewModel, item: item)
+                
+                    .navigationTitle("Courses")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .onAppear{
+                viewModel.getAllCourses()
+            }
         }
-        .onAppear{
-            viewModel.getAllCourses()
-        }
+
+        
+
     }
 }
 
 struct FavoriteView_Previews: PreviewProvider {
     static var previews: some View {
         FavoriteView()
+
     }
 }
 struct FavoriteCell:View{
@@ -41,7 +52,8 @@ struct FavoriteCell:View{
         NavigationLink(item.courseName, destination: CourseDetails(course: item, preRequisite: item.preRequest.components(separatedBy: ",")), isActive: $isActive)
             .swipeActions(edge: .leading, allowsFullSwipe: true, content: {
                 Button(role: .destructive, action: {
-                    action = .edit    // specific action
+                    action = .edit
+                    // specific action
                     //isActive = false
                     viewModel.removeCourse(course: item)
                 } ) {
--- MUST Knowledge base/view/HomeView.swift
+++ MUST Knowledge base/view/HomeView.swift
@@ -36,43 +36,12 @@ struct HomeView: View {
     var courses:[Course]
     var body: some View {
         
-        
-        let drag = DragGesture()
-            .onEnded { _ in
-                withAnimation(.default){
-                    show.toggle()
-                }
-                
-            }
-        
         return  ZStack(alignment: .leading){
             
             
             GeometryReader{_ in
                 VStack{
-                    ZStack{
-                        Button(action: {
-                            show.toggle()
-                            withAnimation(.default){
-                            }
-                        }) {
-                            Image(systemName: "line.3.horizontal")
-                                .resizable()
-                                .renderingMode(.original)
-                                .frame(width: 20, height: 20)
-                            
-                            Spacer()
-                            
-                        }
-                        HStack{
-                            Text("Home")
-                        }
-                        
-                    }
-                    .padding()
-                    .foregroundColor(.primary)
-                    .overlay(Rectangle().stroke(Color.primary.opacity(0.1),lineWidth: 1).shadow( radius: 3).edgesIgnoringSafeArea(.top))
-                    Spacer()
+
                     List(courses,id:\.id) { item in
                         
                         NavigationLink(item.courseName, destination: CourseDetails(course: item, preRequisite: item.preRequest.components(separatedBy: ",")),isActive: $dark)
@@ -96,13 +65,13 @@ struct HomeView: View {
             }
             
             
-            HStack{
-                Menu(dark: $dark, show: $show)
-                    .preferredColorScheme(dark ? .dark : .light)
-                    .offset(x:self.show ? 0 : -UIScreen.main.bounds.width / 1.5)
-                Spacer(minLength: 0)
-                
-            }
+//            HStack{
+//                NavigationMenu(dark: $dark, show: $show)
+//                    .preferredColorScheme(dark ? .dark : .light)
+//                    .offset(x:self.show ? 0 : -UIScreen.main.bounds.width / 1.5)
+//                Spacer(minLength: 0)
+//
+//            }
             .background(Color.primary.opacity(self.show ? (self.dark ? 0.05 : 0.2): 0).edgesIgnoringSafeArea(.all))
             
             
@@ -121,7 +90,7 @@ struct HomeView_Previews: PreviewProvider {
         HomeView(courses: AllFaculties.initFaculties()[0].majors[0].courses)
     }
 }
-struct Menu:View{
+struct NavigationMenu:View{
     @Binding var dark: Bool
     @Binding var show: Bool
     var  body: some View{
@@ -159,7 +128,7 @@ struct Menu:View{
                     Spacer()
                     Text("Dark mode")
                     
-                    //                    Spacer()
+
                     
                 }
                 Toggle("",isOn: $dark)
